e1de425bef17f9596437bb11550f5c3849f923b4
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-10 18:05:25 +0300

    Add model and rename VC

---
+++ MVVM Example/MVVM Example/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVVM Example
+//
+//  Created by Арсентий Халимовский on 10.05.2023.
+//
+
+import Foundation
+
+// MARK: Model - UserData
+struct Person {
+    let name: String
+    let surname: String
+}
